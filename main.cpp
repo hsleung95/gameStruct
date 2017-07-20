0df3aae4b775c2c43fde1f7e46d3431adbd18e8f
Wilson Leung <wilsonleung@Wilsonde-MacBook-Pro.local>
2017-07-20 16:42:11 +0800

    local mac try

--- main.cpp
+++ main.cpp
@@ -39,6 +39,5 @@ int main() {
         }
         cout << endl << "enemy character: Name: " << enemyChar.getName() << ", HP: " << enemyChar.getHP() << ", MP: " << enemyChar.getMP() << ", Attack: " << enemyChar.getAttack() << ", defense: " << enemyChar.getDefense() << endl;
     }
-    cout << "ok?";
     return 0;
 }
